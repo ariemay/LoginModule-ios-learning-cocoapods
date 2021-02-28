829d6bda199f26ad7429dcbf6fdc9097b10fa100
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2021-03-01 03:09:13 +0700

    create module

---
+++ LoginModule/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  LoginModule
+//
+//  Created by Octo Friends on 3/1/21.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
